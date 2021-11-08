4f3e8d2f49f34ba9c7eba8493a991baf388d60ba
Guillermo Murcia <gmurcia@binovo.es>
2021-11-08 12:32:04 +0100

    #8322: Requerir secuencia de factura simpificada para iniciar sesion del TPV si ticketbai esta activado

--- l10n_es_ticketbai_pos/models/pos_config.py
+++ l10n_es_ticketbai_pos/models/pos_config.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Binovo IT Human Project SL
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import base64
-from odoo import models, fields
+from odoo import models, fields, api, exceptions, _
 
 
 class PosConfig(models.Model):
@@ -14,6 +14,9 @@ class PosConfig(models.Model):
     tbai_certificate_id = fields.Many2one(
         comodel_name='tbai.certificate', string='TicketBAI Certificate',
         domain="[('company_id', '=', company_id)]", copy=False)
+    iface_l10n_es_simplified_invoice = fields.Boolean(
+        default=True
+    )
 
     def get_tbai_p12_and_friendlyname(self):
         self.ensure_one()
@@ -31,3 +34,31 @@ class PosConfig(models.Model):
             tbai_p12 = None
             tbai_p12_friendlyname = None
         return tbai_p12, tbai_p12_friendlyname
+
+    @api.multi
+    def open_ui(self):
+        self.ensure_one()
+        if self.tbai_enabled and not self.iface_l10n_es_simplified_invoice:
+            raise exceptions.ValidationError(
+                _("Simplified Invoice IDs Sequence is required")
+            )
+        return super().open_ui()
+
+    @api.multi
+    def open_session_cb(self):
+        self.ensure_one()
+        if self.tbai_enabled and not self.iface_l10n_es_simplified_invoice:
+            raise exceptions.ValidationError(
+                _("Simplified Invoice IDs Sequence is required")
+            )
+        return super().open_session_cb()
+
+    @api.multi
+    def open_existing_session_cb(self):
+        self.ensure_one()
+        if self.tbai_enabled and not self.iface_l10n_es_simplified_invoice:
+            raise exceptions.ValidationError(
+                _("Simplified Invoice IDs Sequence is required")
+            )
+        return super().open_existing_session_cb()
+
